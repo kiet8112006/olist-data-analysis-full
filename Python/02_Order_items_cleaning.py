faeedbf2aab23e334954ef7ef723a891a137fd0d
kiet8112006 <ntkkiet566@gmail.com>
2026-04-12 23:12:36 +0700

    Update 02_Order_items_cleaning.py

--- Python/02_Order_items_cleaning.py
+++ Python/02_Order_items_cleaning.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-Order_items=pd.read_csv('olist_order_items_dataset')
+Order_items=pd.read_csv('olist_order_items_dataset.csv')
 # Check the size of Order_items dataset 
 Order_items_clean=Order_items.copy()
 print(Order_items_clean.shape)
@@ -12,14 +12,14 @@ print(Order_items_clean['product_id'].duplicated().sum())
 print(Order_items_clean['seller_id'].duplicated().sum())
 #  Check price <=0
 print(Order_items_clean['price'] <= 0).sum())
-print(order_items_clean['price'].describe())
+print(Order_items_clean['price'].describe())
 # Check freight_value  <=0
 print((Order_items_clean['freight_value'] <= 0).sum())
 print((Order_items_clean['freight_value'] ==0).sum())
 Order_items_clean['flag_free_shipping']=(Order_items_clean['freight_value']==0).astype(int)
 print(Order_items_clean['freight_value'].describe())
 # Check the correlation between price and freight_value
-print(Order_items_clean[['price', 'freight_value']].corr()
+print(Order_items_clean[['price', 'freight_value']].corr())
 # Save cleaned file
 Order_items_clean.to_csv('Order_items_clean.csv', index= False)
 
