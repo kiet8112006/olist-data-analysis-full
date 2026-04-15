10b710c0aa7754f5b76a5a50ff4c1deea8905fbb
kiet8112006 <ntkkiet566@gmail.com>
2026-04-15 15:34:03 +0700

    Add files via upload

---
+++ 03_Python_cleaning/01_Orders_cleaning (1).py
@@ -0,0 +1,81 @@
+import pandas as pd
+#1. Load to data
+orders = pd.read_csv('olist_orders_dataset.csv')
+orders_clean = orders.copy()
+print("Shape:", orders_clean.shape)
+orders_clean.info()
+#2. Connvert to datetime
+date_cols = [
+    'order_purchase_timestamp',
+    'order_approved_at',
+    'order_delivered_carrier_date',
+    'order_delivered_customer_date',
+    'order_estimated_delivery_date'
+]
+#3. Validate logic 
+for col in date_cols:
+    orders_clean[col] = pd.to_datetime(orders_clean[col], errors='coerce')
+orders_clean['is_invalid_date'] = 0
+
+# Helper function 
+def invalid_condition(cond):
+    return cond.fillna(False)
+
+# Purchase <= Delivered
+orders_clean.loc[
+    invalid_condition(
+        orders_clean['order_purchase_timestamp'] > orders_clean['order_delivered_customer_date']
+    ),
+    'is_invalid_date'
+] = 1
+
+#  Purchase <= Approved
+orders_clean.loc[
+    invalid_condition(
+        orders_clean['order_purchase_timestamp'] > orders_clean['order_approved_at']
+    ),
+    'is_invalid_date'
+] = 1
+
+#  Approved <= Carrier
+orders_clean.loc[
+    invalid_condition(
+        orders_clean['order_approved_at'] > orders_clean['order_delivered_carrier_date']
+    ),
+    'is_invalid_date'
+] = 1
+
+#  Carrier <= Customer
+orders_clean.loc[
+    invalid_condition(
+        orders_clean['order_delivered_carrier_date'] > orders_clean['order_delivered_customer_date']
+    ),
+    'is_invalid_date'
+] = 1
+# Delivered nhưng thiếu ngày giao
+missing_delivered = (
+    (orders_clean['order_status'] == 'delivered') &
+    (orders_clean['order_delivered_customer_date'].isna())
+)
+#4. shipping logic
+# Merge order_items + orders
+df = order_items_clean.merge(
+    orders_clean[['order_id', 'order_purchase_timestamp', 'order_delivered_carrier_date']],
+    on='order_id',
+    how='left'
+)
+#5. Null flag 
+print("Missing delivered date:", missing_delivered.sum())
+orders_clean['flag_approved_null'] = orders_clean['order_approved_at'].isna().astype(int)
+orders_clean['flag_carrier_null'] = orders_clean['order_delivered_carrier_date'].isna().astype(int)
+orders_clean['flag_customer_null'] = orders_clean['order_delivered_customer_date'].isna().astype(int)
+#6. Duplicate 
+print("Duplicate order_id:", orders_clean['order_id'].duplicated().sum())
+print("Duplicate customer_id:", orders_clean['customer_id'].duplicated().sum())
+#7. check order_status
+print(orders_clean['order_status'].value_counts(normalize=True))
+#8. Check range of date
+for col in date_cols:
+    print(f"{col}: {orders_clean[col].min()} → {orders_clean[col].max()}")
+#9. Save 
+orders_clean.to_csv('orders_clean.csv', index=False)
---
+++ 03_Python_cleaning/02_Order_items_cleaning.py
@@ -0,0 +1,29 @@
+import pandas as pd 
+Order_items=pd.read_csv('olist_order_items_dataset.csv')
+# Check the size of Order_items dataset 
+Order_items_clean=Order_items.copy()
+print(Order_items_clean.shape)
+Order_items_clean.info()
+# Check null for all columns
+print(Order_items_clean.isnull().sum())
+# Check duplicate for (order_id, order_item_id), product_id, seller_id 
+print((Order_items_clean.duplicated(subset=['order_id', 'order_item_id']).sum())
+print((Order_items_clean['product_id'].duplicated().sum())
+print((Order_items_clean['seller_id'].duplicated().sum())
+#  Check price <=0
+print(Order_items_clean['price'] <= 0).sum())
+print(Order_items_clean['price'].describe())
+# Check freight_value  <=0
+print((Order_items_clean['freight_value'] <= 0).sum())
+print((Order_items_clean['freight_value'] ==0).sum())
+Order_items_clean['flag_free_shipping']=(Order_items_clean['freight_value']==0).astype(int)
+print((Order_items_clean['freight_value'].describe())
+# Check the correlation between price and freight_value
+print(Order_items_clean[['price', 'freight_value']].corr())
+# Convert shipping_limit_date into datetime column
+Order_items_clean['shipping_limit_date']=pd.to_datetime(Order_items_clean['shipping_limit_date'], errors='coerce')
+# Check range of shipping_limit_date
+print('Max shipping_limit_date :', Order_items_clean['shipping_limit_date'].max(), 'and Min shipping_limit_date :', Order_items_clean['shipping_limit_date'].min())
+# Save cleaned file
+Order_items_clean.to_csv('Order_items_clean.csv', index= False)
+
