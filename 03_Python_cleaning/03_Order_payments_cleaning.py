79a40abf879108c36b73a83a7f9da4b291b222a4
kiet8112006 <ntkkiet566@gmail.com>
2026-04-15 15:35:43 +0700

    Add files via upload

---
+++ 03_Python_cleaning/03_Order_payments_cleaning.py
@@ -0,0 +1,25 @@
+import pandas as pd
+Order_payments=pd.read_csv('olist_order_payments_dataset.csv')
+# Check the size of Order_payments dataset
+Order_payments_clean=Order_payments.copy()
+print(Order_payments_clean.shape)
+Order_payments_clean.info()
+# Check null for all columns
+cols=['order_id', 'payment_sequential', 'payment_type', 'payment_installments', 'payment_value']
+for i in cols:
+ print(Order_payments_clean[i].isnull().sum())
+# Check duplicate 
+print(Order_payments_clean.duplicated(subset=['order_id', 'payment_sequential']).sum())
+# Check different values in Payment_type
+print(Order_payments_clean['payment_type'].value_counts(normalize=True))
+# Check payment_installments <= 0
+print((Order_payments_clean['payment_installments'] <=0 ).sum())
+print(Order_payments_clean['payment_installments'].describe())
+Order_payments_clean['flag_invalid_installments']=(Order_payments_clean['payment_installments'] <= 0 ).astype(int)
+# Check  payment_value 
+print(Order_payments_clean['payment_value'].describe())
+print((Order_payments_clean['payment_value'] ==0).sum())
+Order_payments_clean['flag_payment_value']=(Order_payments_clean['payment_value'] ==0).astype(int)
+# save cleaned file
+Order_payments_clean.to_csv('Order_payments_clean.csv', index= False)
+
---
+++ 03_Python_cleaning/05_Customer_cleaning.py
@@ -0,0 +1,42 @@
+import pandas as pd 
+Customers=pd.read_csv('olist_customers_dataset.csv')
+# Check the size of Customers dataset 
+Customers_clean=Customers.copy()
+print(Customers_clean.shape)
+# Check null for all columns
+cols =['customer_id', 'customer_unique_id', 'customer_state', 'customer_city', 'customer_zip_code_prefix']
+for i in cols:
+    print(f"{i} : {Customers_clean[i].isnull().sum()}")
+# Check duplicates
+print('customer_id : ',Customers_clean['customer_id'].duplicated().sum())
+print('customer_unique_id : ',Customers_clean['customer_unique_id'].duplicated().sum())
+# standardize customer_state
+Customers_clean['customer_state']=Customers_clean['customer_state'].str.strip().str.upper()
+print(Customers_clean['customer_state'].value_counts(normalize=True))
+for letter in sorted(Customers_clean['customer_state'].str[0].unique()):
+    print(f"\n--- {letter} ---")
+    print(sorted(Customers_clean[Customers_clean['customer_state'].str.startswith(letter)]['customer_state'].unique()))
+# standardize customer_city 
+print(Customers_clean['customer_city'].value_counts(normalize=True))
+import unicodedata
+def remove_accent(text):
+    return ''.join(
+        c for c in unicodedata.normalize('NFKD', text)
+        if unicodedata.category(c) != 'Mn'
+    )
+Customers_clean['customer_city'] = Customers_clean['customer_city'].apply(remove_accent)
+Customers_clean['customer_city'] = (
+    Customers_clean['customer_city']
+    .str.strip()                         
+    .str.upper()                       
+    .apply(remove_accent)                
+    .str.replace(r'\s+', ' ', regex=True) 
+)
+# Check customer_zip_code_prefix
+Customers_clean['customer_zip_code_prefix'].info()
+Customers_clean['customer_zip_code_prefix'] = (
+    Customers_clean['customer_zip_code_prefix']
+    .astype(str)
+    .str.zfill(5))
+# Save cleaned file
+Customers_clean.to_csv('Customers_clean.csv')
