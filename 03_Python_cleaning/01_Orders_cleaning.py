01cd4e88afe3293d23c9269e86063f24bb7ed712
kiet8112006 <ntkkiet566@gmail.com>
2026-04-15 16:26:03 +0700

    Update 01_Orders_cleaning.py

--- 03_Python_cleaning/01_Orders_cleaning.py
+++ 03_Python_cleaning/01_Orders_cleaning.py
@@ -1,10 +1,13 @@
 import pandas as pd
-#1. Load to data
-orders = pd.read_csv('olist_orders_dataset.csv')
-orders_clean = orders.copy()
-print("Shape:", orders_clean.shape)
-orders_clean.info()
-#2. Connvert to datetime
+
+# 1. Load data
+Orders = pd.read_csv('olist_orders_dataset.csv')
+Orders_clean = Orders.copy()
+
+print("Shape:", Orders_clean.shape)
+Orders_clean.info()
+
+# 2. Convert to datetime
 date_cols = [
     'order_purchase_timestamp',
     'order_approved_at',
@@ -12,63 +15,72 @@ date_cols = [
     'order_delivered_customer_date',
     'order_estimated_delivery_date'
 ]
-#3. Validate logic 
-for col in date_cols:
-    orders_clean[col] = pd.to_datetime(orders_clean[col], errors='coerce')
-orders_clean['is_invalid_date'] = 0
 
-# Helper function 
+for col in date_cols:
+    Orders_clean[col] = pd.to_datetime(Orders_clean[col], errors='coerce')
+
+# 3. Validate logic
+Orders_clean['is_invalid_date'] = 0
+
+# Helper function
 def invalid_condition(cond):
     return cond.fillna(False)
 
 # Purchase <= Delivered
-orders_clean.loc[
+Orders_clean.loc[
     invalid_condition(
-        orders_clean['order_purchase_timestamp'] > orders_clean['order_delivered_customer_date']
+        Orders_clean['order_purchase_timestamp'] > Orders_clean['order_delivered_customer_date']
     ),
     'is_invalid_date'
 ] = 1
 
-#  Purchase <= Approved
-orders_clean.loc[
+# Purchase <= Approved
+Orders_clean.loc[
     invalid_condition(
-        orders_clean['order_purchase_timestamp'] > orders_clean['order_approved_at']
+        Orders_clean['order_purchase_timestamp'] > Orders_clean['order_approved_at']
     ),
     'is_invalid_date'
 ] = 1
 
-#  Approved <= Carrier
-orders_clean.loc[
+# Approved <= Carrier
+Orders_clean.loc[
     invalid_condition(
-        orders_clean['order_approved_at'] > orders_clean['order_delivered_carrier_date']
+        Orders_clean['order_approved_at'] > Orders_clean['order_delivered_carrier_date']
     ),
     'is_invalid_date'
 ] = 1
 
-#  Carrier <= Customer
-orders_clean.loc[
+# Carrier <= Customer
+Orders_clean.loc[
     invalid_condition(
-        orders_clean['order_delivered_carrier_date'] > orders_clean['order_delivered_customer_date']
+        Orders_clean['order_delivered_carrier_date'] > Orders_clean['order_delivered_customer_date']
     ),
     'is_invalid_date'
 ] = 1
-# Delivered nhưng thiếu ngày giao
+
+# 4. Delivered nhưng thiếu ngày giao
 missing_delivered = (
-    (orders_clean['order_status'] == 'delivered') &
-    (orders_clean['order_delivered_customer_date'].isna())
+    (Orders_clean['order_status'] == 'delivered') &
+    (Orders_clean['order_delivered_customer_date'].isna())
 )
-#5. Null flag 
+
 print("Missing delivered date:", missing_delivered.sum())
-orders_clean['flag_approved_null'] = orders_clean['order_approved_at'].isna().astype(int)
-orders_clean['flag_carrier_null'] = orders_clean['order_delivered_carrier_date'].isna().astype(int)
-orders_clean['flag_customer_null'] = orders_clean['order_delivered_customer_date'].isna().astype(int)
-#6. Duplicate 
-print("Duplicate order_id:", orders_clean['order_id'].duplicated().sum())
-print("Duplicate customer_id:", orders_clean['customer_id'].duplicated().sum())
-#7. check order_status
-print(orders_clean['order_status'].value_counts(normalize=True))
-#8. Check range of date
+
+# 5. Null flag
+Orders_clean['flag_approved_null'] = Orders_clean['order_approved_at'].isna().astype(int)
+Orders_clean['flag_carrier_null'] = Orders_clean['order_delivered_carrier_date'].isna().astype(int)
+Orders_clean['flag_customer_null'] = Orders_clean['order_delivered_customer_date'].isna().astype(int)
+
+# 6. Duplicate
+print("Duplicate order_id:", Orders_clean['order_id'].duplicated().sum())
+print("Duplicate customer_id:", Orders_clean['customer_id'].duplicated().sum())
+
+# 7. Check order_status
+print(Orders_clean['order_status'].value_counts(normalize=True))
+
+# 8. Check range of date
 for col in date_cols:
-    print(f"{col}: {orders_clean[col].min()} → {orders_clean[col].max()}")
-#9. Save 
-orders_clean.to_csv('orders_clean.csv', index=False)
+    print(f"{col}: {Orders_clean[col].min()} → {Orders_clean[col].max()}")
+
+# 9. Save
+Orders_clean.to_csv('orders_clean.csv', index=False)
